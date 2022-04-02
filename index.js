4a810fff18c7841da5392e8595718fbea6754733
ljjkerwin <ljjkerwin@gmail.com>
2022-04-02 12:22:32 +0800

    init

---
+++ index.js
@@ -0,0 +1,20 @@
+const Koa = require('koa')
+const app = new Koa()
+
+
+const port = 3000
+const text = process.argv[2]
+
+
+app.use(async (ctx, next) => {
+    const now = new Date()
+    const result = `${text}: ${now}`
+    console.log(result)
+    ctx.body = result
+})
+
+
+
+app.listen(port, () => {
+    console.log('listening on port', port)
+})
